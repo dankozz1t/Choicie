b2f2e0f0828cbfffe8327187c8aa3a592b6dff00
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-10-27 22:10:05 +0300

    add video tag

--- src/modules/Main/Home/AboutSection/AboutSection.jsx
+++ src/modules/Main/Home/AboutSection/AboutSection.jsx
@@ -10,10 +10,11 @@ const AboutSection = () => {
     <div className="about" id="about">
       <Container className="about__container">
         <h2 className="about__title">Learn more about Choicie in 1 minute</h2>
-        <img
-          src={aboutImage}
-          alt="Person made food delivery and eats on the balcony"
-        />
+
+        <video width="1170" height="650" poster={aboutImage} controls>
+          <source src="#" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
       </Container>
     </div>
   );
