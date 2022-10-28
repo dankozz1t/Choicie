8f0fe5f592ed68a981269282c4b6d7acd20f35d8
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-10-28 13:53:53 +0300

    add sections

--- src/pages/ClientPage/ClientPage.jsx
+++ src/pages/ClientPage/ClientPage.jsx
@@ -6,7 +6,13 @@ import ClientHeroSection from "modules/Main/Client/ClientHeroSection";
 import clientNavigationItems from "./clientNavigationItems.json";
 
 import RestaurantsTrustSection from "modules/Main/Client/RestaurantsTrustSection";
+import DashboardSection from "modules/Main/Client/DashboardSection";
+import StagesDevelopmentSection from "modules/Main/Client/StagesDevelopmentSection";
+import ReviewSection from "modules/Main/Client/ReviewSection/ReviewSection";
+import SubscribeSection from "modules/Main/SubscribeSection";
 import Footer from "modules/Footer";
+
+import "./ClientPage.scss";
 
 const ClientPage = () => {
   return (
@@ -15,6 +21,10 @@ const ClientPage = () => {
       <main>
         <ClientHeroSection />
         <RestaurantsTrustSection />
+        <DashboardSection />
+        <StagesDevelopmentSection />
+        <ReviewSection />
+        <SubscribeSection />
       </main>
       <Footer />
     </div>
