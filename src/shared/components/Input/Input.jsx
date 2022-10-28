47d2bc0e90e136a618e55f939bf5a57d139017c6
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-10-28 21:08:49 +0300

    update classNames

--- src/shared/components/Input/Input.jsx
+++ src/shared/components/Input/Input.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import classNames from "classnames";
 
 import "./Input.scss";
 
 const Input = ({ className, ...props }) => {
-  return <input className={`input ${className}`} {...props} />;
+  return <input className={classNames("input", className)} {...props} />;
 };
 
 export default Input;
