009c6ca421bf2194e541a4a14035adac4c04d762
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-10-28 19:51:16 +0300

    add adaptive for SocialNetworkSection

--- src/modules/Main/Home/SocialNetworkSection/SocialNetworkSection.jsx
+++ src/modules/Main/Home/SocialNetworkSection/SocialNetworkSection.jsx
@@ -1,15 +1,29 @@
 import React from "react";
+import { useMediaQuery } from "react-responsive";
+
+import Container from "shared/components/Container";
+import Button from "shared/components/Button";
 
 import socialNetworkImage from "assets/images/SocialNetworkSection/socialNetwork-desktop@1x.png";
+import { ReactComponent as SocialNetworkMobileIcon } from "assets/icons/SocialNetworkSection/01_social-network.svg";
 
 import "./SocialNetworkSection.scss";
-import Button from "shared/components/Button";
-import Container from "shared/components/Container";
 
 const SocialNetworkSection = () => {
+  const isDesktop = useMediaQuery({ minWidth: 1728 });
+
   return (
     <section className="social-network">
       <Container className="social-network__container">
+        {isDesktop ? (
+          <img
+            src={socialNetworkImage}
+            alt="Person made food delivery and eats on the balcony"
+          />
+        ) : (
+          <SocialNetworkMobileIcon className="social-network__mobile-icon" />
+        )}
+
         <div className="social-network__wrapper">
           <h2 className="social-network__title">And a new social network</h2>
 
@@ -22,15 +36,10 @@ const SocialNetworkSection = () => {
             the dishes you liked and didn't like, and the foods you're allergic
             to.
           </p>
-          <Button primary medium>
+          <Button primary medium className="social-network__button">
             Get Started
           </Button>
         </div>
-
-        <img
-          src={socialNetworkImage}
-          alt="Person made food delivery and eats on the balcony"
-        />
       </Container>
     </section>
   );
