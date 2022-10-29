b4b1fb5d48cc13f26f2fe84ec31b19ec45032967
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-10-29 10:34:06 +0300

    add adaptive for ClientHeroSection

--- src/modules/Main/Client/ClientHeroSection/ClientHeroSection.jsx
+++ src/modules/Main/Client/ClientHeroSection/ClientHeroSection.jsx
@@ -1,14 +1,20 @@
 import React from "react";
+import { useMediaQuery } from "react-responsive";
 
 import Container from "shared/components/Container";
 import Comment from "shared/components/Comment";
 
-import heroImage from "assets/images/ClientHeroSection/client-hero-desktop@1x.png";
+import heroDesktop1xImage from "assets/images/ClientHeroSection/client-hero-desktop@1x.png";
+import heroDesktop2xImage from "assets/images/ClientHeroSection/client-hero-desktop@2x.png";
+import heroMobileImage from "assets/images/ClientHeroSection/client-hero-mobile@1x.png";
+
 import { ReactComponent as positiveIcon } from "assets/icons/Comment/01_positive-review.svg";
 
 import "./ClientHeroSection.scss";
 
 const ClientHeroSection = () => {
+  const isDesktop = useMediaQuery({ minWidth: 768 });
+
   return (
     <section className="client-hero">
       <Container className="client-hero__container">
@@ -27,11 +33,26 @@ const ClientHeroSection = () => {
           </a>
         </div>
         <div>
-          <img src={heroImage} alt="the waiter writes down the order" />
+          <picture>
+            <source
+              srcSet={`${heroDesktop1xImage} 1x, ${heroDesktop2xImage} 2x`}
+              media="(min-width:1728px)"
+            />
+            <source
+              srcSet={`${heroMobileImage} 1x`}
+              media="(max-width:1000px)"
+            />
+
+            <img src={heroMobileImage} alt="the waiter writes down the order" />
+          </picture>
 
           <Comment
             className="client-hero__comment"
-            title=" “We edited the menu after using this app. Now our sales are 30% higher“"
+            title={
+              isDesktop
+                ? "“We edited the menu after using this app. Now our sales are 30% higher“"
+                : "“Thanks to Choicie, we edited the menu, which resulted in 30% increase in sales“"
+            }
             name="Anastasia Glorman,"
             Icon={positiveIcon}
             coFounder="co-fouder @syngenta"
