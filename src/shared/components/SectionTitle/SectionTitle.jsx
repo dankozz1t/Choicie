27246baef6161be4de0a20d14a95d7f612e5778f
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-11-01 09:38:25 +0200

    fix classes order

--- src/shared/components/SectionTitle/SectionTitle.jsx
+++ src/shared/components/SectionTitle/SectionTitle.jsx
@@ -6,7 +6,7 @@ import "./SectionTitle.scss";
 const SectionTitle = ({ children, className, tag = "h2" }) => {
   const CustomTag = tag;
   return (
-    <CustomTag className={classNames("section-title", className)}>
+    <CustomTag className={classNames(className, "section-title")}>
       {children}
     </CustomTag>
   );
